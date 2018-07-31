d291e307a60f13c1c2c9d28a07b77488ec0696a7
Marshall Parker <marshall@blackarcsecurity.com>
2018-07-31 15:56:52 -0500

    Change hashing algorithm to X16R
    	modified:   src/Makefile.am

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -12,7 +12,7 @@
 
 uint256 CBlockHeader::GetHash() const
 {
-    return HashX11(BEGIN(nVersion), END(nNonce));
+	return HashX16R(BEGIN(nVersion), END(nNonce), hashPrevBlock);
 }
 
 std::string CBlock::ToString() const
