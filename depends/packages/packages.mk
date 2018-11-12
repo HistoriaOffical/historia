95cecf032270a7c079b446a2838902c421dc630b
Marshall Parker <marshall@blackarcsecurity.com>
2018-11-12 14:58:45 -0600

    IPFS Implementation

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -8,7 +8,7 @@ qt_packages = qrencode protobuf
 
 qt_linux_packages= qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
 qt_darwin_packages=qt
-qt_mingw32_packages=qt
+qt_mingw32_packages=qt curl
 
 
 wallet_packages=bdb
