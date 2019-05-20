3afe8e1920a0978b8b9175b4ef4fe0d7b2aaf7ef
marshalljp <marshall@historia.network>
2019-05-20 13:53:16 -0500

    Add natural reduction of coin supply

--- src/validation.cpp
+++ src/validation.cpp
@@ -1254,12 +1254,11 @@ CAmount GetBlockSubsidy(int nPrevBits, int nPrevHeight, const Consensus::Params&
     // LogPrintf("height %u diff %4.2f reward %d\n", nPrevHeight, dDiff, nSubsidyBase);
     CAmount nSubsidy = nSubsidyBase * COIN;
     
-    /*
     // yearly decline of production by ~7.1% per year, projected ~18M coins max by year 2050+.
     for (int i = consensusParams.nSubsidyHalvingInterval; i <= nPrevHeight; i += consensusParams.nSubsidyHalvingInterval) {
-        nSubsidy -= nSubsidy/14;
+        nSubsidy -= nSubsidy/13;
     }
-    */
+    
     // Hard fork to reduce the block reward by 20 extra percent (allowing budget/superblocks/communityfunding)
     CAmount nSuperblockPart = (nPrevHeight > consensusParams.nBudgetPaymentsStartBlock) ? nSubsidy/5 : 0;
 
