1e0a9f044b31da3df875039984ee8343391aedc0
marshalljp <marshall@historia.network>
2019-07-09 10:56:32 -0500

    Update CDMN start block

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -107,8 +107,8 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].bit = 28;
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nStartTime = 1199145601; // January 1, 2008
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nTimeout = 1230767999;   // December 31, 2008
-        consensus.nIpfsEnforceBlock = 151000;
-        consensus.nSplitMasternodeBlock = 151000;
+        consensus.nIpfsEnforceBlock = 162500;
+        consensus.nSplitMasternodeBlock = 162500;
         
         // Deployment of BIP68, BIP112, and BIP113.
         consensus.vDeployments[Consensus::DEPLOYMENT_CSV].bit = 0;
