5d36b689d4fc7d133534ae662b2a3f146d1b50a7
marshalljp <marshall@historia.network>
2019-08-29 12:07:24 -0500

    Update Ports

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -34,7 +34,7 @@ class CBaseMainParams : public CBaseChainParams
 public:
     CBaseMainParams()
     {
-        nRPCPort = 9998;
+        nRPCPort = 10100;
     }
 };
 static CBaseMainParams mainParams;
@@ -47,7 +47,7 @@ class CBaseTestNetParams : public CBaseChainParams
 public:
     CBaseTestNetParams()
     {
-        nRPCPort = 19998;
+        nRPCPort = 35776;
         strDataDir = "testnet3";
     }
 };
@@ -61,7 +61,7 @@ class CBaseDevNetParams : public CBaseChainParams
 public:
     CBaseDevNetParams(const std::string &dataDir)
     {
-        nRPCPort = 19998;
+        nRPCPort = 35776;
         strDataDir = dataDir;
     }
 };
