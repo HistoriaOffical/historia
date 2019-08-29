50370b8f1ca7aaf2faa567e24f0cc3d4df818a9e
marshalljp <marshall@historia.network>
2019-08-29 12:22:58 -0500

    Remove unneed translations (for now)

--- src/qt/locale/historia_en.ts
+++ src/qt/locale/historia_en.ts
@@ -1844,10 +1844,8 @@
     </message>
     <message>
         <location line="+21"/>
-        <source>Language missing or translation incomplete? Help contributing translations here:
-https://www.transifex.com/projects/p/historia/</source>
-        <translation>Language missing or translation incomplete? Help contributing translations here:
-https://www.transifex.com/projects/p/historia/</translation>
+        <source>More translations coming soon!</source>
+        <translation>More translations coming soon!</translation>
     </message>
     <message>
         <location line="+19"/>
