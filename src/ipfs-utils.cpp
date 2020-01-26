8a37aff18c877cf13beff6c88eae1eee9397225d
marshalljp <marshall@historia.network>
2020-01-26 15:30:16 -0600

    Only do IPFS Peer ID validation on 5000 level masternodes

--- src/ipfs-utils.cpp
+++ src/ipfs-utils.cpp
@@ -4,24 +4,26 @@
 
 #include "json.hpp"
 
-bool IsIpfsPeerIdValid(const std::string& ipfsId, CAmount collateralAmount) {
+bool IsIpfsPeerIdValid(const std::string& ipfsId, CAmount collateralAmount)
+{
     /** All alphanumeric characters except for "0", "I", "O", and "l" */
     std::string base58chars =
-	"123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+        "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
 
-    if (ipfsId == "0" && collateralAmount != 100 * COIN)
-	return false;
+    if (ipfsId == "0" && collateralAmount != 100 * COIN && ipfsId == "")
+        return false;
     
     /** https://docs.ipfs.io/guides/concepts/cid/ CID v0 */
-    if (ipfsId.size() != 46 || ipfsId[0] !='Q' || ipfsId[1] !='m') {
-	return false;
+    if (collateralAmount == 5000 * COIN) {
+        if (ipfsId.size() != 46 || ipfsId[0] != 'Q' || ipfsId[1] != 'm') {
+            return false;
+        }
+
+        int l = ipfsId.length();
+        for (int i = 0; i < l; i++)
+            if (base58chars.find(ipfsId[i]) == -1)
+                return false;
     }
-
-    int l = ipfsId.length();
-    for (int i = 0; i < l; i++)
-	if (base58chars.find(ipfsId[i]) == -1)
-	    return false;
-
     return true;
 }
 
