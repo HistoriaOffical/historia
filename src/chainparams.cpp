88e2711d89e0f87b724a02c3cf1522b43991dea4
marshall <marshall@historia.network>
2020-01-30 14:16:19 +0000

    Make sure X16Rv2 switch is before DIP3 enforcement block

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -388,9 +388,9 @@ public:
         consensus.BIP65Height = 2; // 0000039cf01242c7f921dcb4806a5994bc003b48c1973ae0c89b67809c2bb2ab
         consensus.BIP66Height = 3; // 0000002acdd29a14583540cb72e1c5cc83783560e38fa7081495d474fe1671f7
         consensus.DIP0001Height = 4;
-        consensus.DIP0003EnforcementHeight = 6;
+        consensus.DIP0003EnforcementHeight = 398;
         consensus.DIP0003Height = 5;
-        consensus.DIP0003EnforcementHash = uint256S("0000029a7da674caaa824c81e4f8bced0d42057ad73e57b773f587592615dbe6");
+        consensus.DIP0003EnforcementHash = uint256S("00000f6bc336239f3f2b2ed4ad2ba45f3cc5affd6c91ccd8d9ddf998b08afa4b");
         consensus.powLimit = uint256S("00000fffff000000000000000000000000000000000000000000000000000000"); // ~uint256(0) >> 20
         consensus.nPowTargetTimespan = 24 * 60 * 60; // Historia: 1 day
         consensus.nPowTargetSpacing = 2.5 * 60; // Historia: 2.5 minutes
