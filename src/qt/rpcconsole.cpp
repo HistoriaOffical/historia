dd3887b586bde74e78535fa27c9cfd6213164c89
marshall <marshall@historia.network>
2020-02-09 16:38:06 +0000

    Fix GenVote Keys being enabled before synced

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -765,7 +765,7 @@ void RPCConsole::setClientModel(ClientModel *model)
 
         ui->btn_sendvotingnodetx->setDisabled(true);
         ui->btn_sendprotx->setDisabled(true);
-        ui->btn_genvoterkeys->setDisabled(false);
+        ui->btn_genvoterkeys->setDisabled(true);
         ui->btn_revokevotingnode->hide();
         ui->btn_revokevotingnode->setDisabled(true);
         populateAdditionalInfo(-1, "");
